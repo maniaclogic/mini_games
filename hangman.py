8aceaf589d33782317d43d62f8f675f02333cbcb
Kat <katjabensiek@gmail.com>
2019-08-07 22:24:26 +0200

    more fucntionality added to function

--- hangman.py
+++ hangman.py
@@ -4,4 +4,14 @@ words = open('words.txt', 'r').readlines()
 words = [word.replace('\n', ' ').replace('\"', ' ').strip() for word in words]
 
 def play_hangman():
-    
+    hangman_word = words[random.randint(0, len(words) - 1)]
+    underscore_array = ['_' for i in hangman_word]
+
+    game_end = false
+
+    while game_end == false:
+        print(*underscore_array, sep=' ')
+
+        player_move = input('Enter your guess! (A - Z)')
+
+        
