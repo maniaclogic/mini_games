8b0e1b16fbe16244926fa69d544cb51eb7586e35
maniaclogic <katjabensiek@gmail.com>
2022-01-12 14:55:50 +0100

    index of median

--- katas/katas.py
+++ katas/katas.py
@@ -4,9 +4,15 @@ def divisors(n):
     list_of_divs = [div for div in range(1, n+1) if n % div == 0]
     return len(list_of_divs)
 
-# Learnings: sum is fast so sum(1 for div in range(1, n+1) if n % div == 0) is better
+# Learnings: sum and xrange are faster, so sum(1 for div in xrange(1, n+1) if n % div == 0) is better
 # # also could have used lambda
 
 
 #####################################################
-## 
+## Create a function that returns the index of the 
+## numerical element that lies between two other elements.
+
+def index_of_median(three_nums):
+    return three_nums.index(sorted(three_nums)[1])
+
+# Learnings: you can define a function like this gimme=lambda l:l.index(sorted(l)[1])
--- katas/test_katas.py
+++ katas/test_katas.py
@@ -1,5 +1,5 @@
 import pytest 
-from katas import divisors
+from katas import divisors, index_of_median
 
 def test_divisors():
     assert divisors(1) == 1
@@ -8,3 +8,7 @@ def test_divisors():
     assert divisors(12) == 6
     assert divisors(30) == 8
     assert divisors(4096) == 13
+
+def test_index_of_median():
+    assert index_of_median([2, 3, 1]) == 0
+    assert index_of_median([2, 14, 10]) == 2
